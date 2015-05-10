2f1546f974d12129b6ee8f8954399e9b7880009b
Adrian L Lange <mail@p3lim.net>
2015-05-10 10:55:53 +0200

    Add a CheckButton widget

---
+++ widgets/CheckButton.lua
@@ -0,0 +1,29 @@
+local widgetType, widgetVersion = 'CheckButton', 1
+local Wasabi = LibStub and LibStub('Wasabi', true)
+if(not Wasabi or (Wasabi:GetWidgetVersion(widgetType) or 0) >= widgetVersion) then
+	return
+end
+
+local methods = {}
+function methods:Update(value)
+	self:SetChecked(value)
+end
+
+local function OnClick(self)
+	self.panel.temp[self.key] = self:GetChecked()
+end
+
+Wasabi:RegisterWidget(widgetType, widgetVersion, function(panel, key)
+	local Button = CreateFrame('CheckButton', nil, panel, 'InterfaceOptionsCheckButtonTemplate')
+	Button:SetScript('OnClick', OnClick)
+	Button.panel = panel
+	Button.key = key
+
+	for method, func in next, methods do
+		Button[method] = func
+	end
+
+	panel.objects[key] = Button
+
+	return Button
+end)
